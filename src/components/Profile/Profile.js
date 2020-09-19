5defa3db687821b324e1c9bdae39d5c4c128755f
massqeen <62159695+massqeen@users.noreply.github.com>
2020-09-20 01:45:33 +0300

    added not working default props and prop-types

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,11 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Stats from "../Stats/Stats";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Stats from '../Stats/Stats';
+// import './default.jpg';
 
-const Profile = ({user}) => {
-const {name,tag,location,avatar}=user
+const Profile = (user) => {
+    console.log(user);
+    const {name, tag, location, avatar} = user
     return (<div className="profile">
         <div className="description">
             <img
@@ -18,4 +20,23 @@ const {name,tag,location,avatar}=user
         <Stats stats={user.stats}/>;
     </div>);
 };
+Profile.defaultProps = {
+    user:{
+        name: "test",
+        tag: "",
+        location: "",
+        avatar: "./default.jpg"
+    }
+
+};
+
+Profile.propTypes = {
+    optionalObjectWithStrictShape: PropTypes.exact({
+        name: PropTypes.string.isRequired,
+        tag: PropTypes.string,
+        location: PropTypes.string,
+        avatar: PropTypes.string
+    })
+};
+
 export default Profile;
