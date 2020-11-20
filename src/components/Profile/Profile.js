e5f18dab7cda34c2425d604217ccc48e8adb9f4c
massqeen <62159695+massqeen@users.noreply.github.com>
2020-11-20 03:09:05 +0200

    updated npm, added prettier; replaced not working image links with alts; reformatted few files with prettier

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,39 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Profile.module.css';
 import Stats from '../Stats/Stats';
-import img from '../default.jpg';
 
-const Profile = ({name, tag, location, avatar, stats}) => {
-    return (
-        <div className={styles.profile} >
-            <div className={styles.profileWrapper}>
-                <div className={styles.description}>
-                    <img src={avatar} alt="user avatar" className={styles.avatar} width="128"/>
-                    <p className={styles.name}>{name}</p>
-                    <p className={styles.tag}>@{tag}</p>
-                    <p className={styles.location}>{location}</p>
-                </div>
-                <Stats
-                    followers={stats.followers}
-                    likes={stats.likes}
-                    views={stats.views}
-                />
-            </div>
+const Profile = ({ name, tag, location, avatar, stats }) => {
+  return (
+    <div className={styles.profile}>
+      <div className={styles.profileWrapper}>
+        <div className={styles.description}>
+          <img
+            src={avatar}
+            alt="user avatar"
+            className={styles.avatar}
+            width="128"
+          />
+          <p className={styles.name}>{name}</p>
+          <p className={styles.tag}>@{tag}</p>
+          <p className={styles.location}>{location}</p>
         </div>
-    );
+        <Stats
+          followers={stats.followers}
+          likes={stats.likes}
+          views={stats.views}
+        />
+      </div>
+    </div>
+  );
 };
 Profile.defaultProps = {
-    tag: '',
-    location: '',
-    avatar: img
+  tag: '',
+  location: 'Earth',
 };
 
 Profile.propTypes = {
-    name: PropTypes.string.isRequired,
-    tag: PropTypes.string,
-    location: PropTypes.string,
-    avatar: PropTypes.string,
-    stats: PropTypes.object
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.object,
 };
 
 export default Profile;
